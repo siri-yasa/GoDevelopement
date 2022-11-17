4b83e9b617ec87d63d89923ee8e8eedf15442058
siri-yasa <sireesha.yasa08@gmail.com>
2022-11-16 19:15:47 -0600

    python files

---
+++ python/string.py
@@ -0,0 +1,11 @@
+course = "hello world"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+first = "siri"
+last = "yasa"
+full = first + " " +last
+print(full)
---
+++ python/stringfunc.py
@@ -0,0 +1,7 @@
+course = "  hello world"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
